e042ebcec12fff072d22dba20c6720d45188df9a
Aakash-agnan001 <aakashappiah@gmail.com>
2022-09-05 13:12:04 -0700

    Completed all functionality of application

--- workout-tracker/backend/index.js
+++ workout-tracker/backend/index.js
@@ -1,8 +1,30 @@
+require('dotenv').config()
+
 const express = require("express")
-
+// express app
 const app = express()
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
-SERVER_PORT = 4000
-app.listen(SERVER_PORT, () => {
-    console.log(`Listening to Port ${SERVER_PORT}\n`)
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+//connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Connected to DB and Listening to Port ${process.env.PORT}\n`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
